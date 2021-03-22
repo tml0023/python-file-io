1840ab5381d88824a389062a905c61fa0b72ad56
Troy LoBue <troylobue33@gmail.com>
2021-03-22 15:11:25 -0500

    I am trying to get the key branch open.

---
+++ dummy.py
@@ -0,0 +1,11 @@
+with open('dummy.txt', 'r') as in_stream:
+    with open('output.txt', 'w') as out_stream:
+        for line in in_stream:
+            line = line.strip()
+            word_list = line.split()
+            word_list.sort()
+            for word in word_list:
+                out_stream.write('{0}\n'.format(word))
+print('Done')
+print('dumy.txt closed/?', in_stream.closed)
+print('output.txt closed?', out_stream.closed)
