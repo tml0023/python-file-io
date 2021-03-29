7c8e342e929a7cddf03d104b2d69fab9bd34b0c1
Troy LoBue <troylobue33@gmail.com>
2021-03-29 15:06:02 -0500

    here we add the exercise files

--- dummy.py
+++ dummy.py
@@ -1,4 +1,6 @@
+print('Opening dummy.txt')
 with open('dummy.txt', 'r') as in_stream:
+    print('Opening output.txt')
     with open('output.txt', 'w') as out_stream:
         for line in in_stream:
             line = line.strip()
@@ -6,6 +8,6 @@ with open('dummy.txt', 'r') as in_stream:
             word_list.sort()
             for word in word_list:
                 out_stream.write('{0}\n'.format(word))
-print('Done')
-print('dumy.txt closed/?', in_stream.closed)
-print('output.txt closed?', out_stream.closed)
+print("Done!")
+print('dummy.txt is closed?', in_stream.closed)
+print('output.txt is closed?', out_stream.closed)
